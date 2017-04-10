ec382a49ba4bc48dd1b3aabc7445b7093aeecf43
ryodream <ryodream@github.com>
2017-04-10 15:48:19 -0400

    [feature] Added script to statistic crimes in each weekday

---
+++ scripts/datetime/spark_crime_amount_each_weekday.py
@@ -0,0 +1,23 @@
+from __future__ import print_function
+
+import sys
+from csv import reader
+from operator import add
+from pyspark import SparkContext
+from datetime import datetime
+
+def format_date(date_str):
+    crime_date = datetime.strptime(date_str, '%m/%d/%Y')
+    return crime_date.weekday()
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: spark_crime_amount_each_weekday.py <file>", file=sys.stderr)
+        exit(-1)
+    sc = SparkContext()
+    lines = sc.textFile(sys.argv[1], 1)
+    lines = lines.mapPartitions(lambda x : reader(x)).map(lambda row: row[1])
+    counts = lines.map(format_date).map(lambda crime_date : (crime_date, 1)).reduceByKey(add)
+    counts = counts.map(lambda row : str(row[0])+"\t"+str(row[1]))
+    counts.saveAsTextFile("crime_amount_each_weekday.out")
+    sc.stop()
