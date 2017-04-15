9fb4179c6b805b4efde685f11c97e8a0f440f5eb
ryodream <shiyanch@gmail.com>
2017-04-15 15:02:07 -0400

    [update] Fixed a bug on null date

--- scripts/cleaning/spark_replace_dawn_hour.py
+++ scripts/cleaning/spark_replace_dawn_hour.py
@@ -12,9 +12,10 @@ def replace_dawn_hour(row):
     for index in (2, 4):
         if row[index] == '24:00:00':
             row[index] = '00:00:00'
-            crime_date = datetime.strptime(row[index-1], '%m/%d/%Y')
-            crime_date += timedelta(days=1)
-            row[index-1] = crime_date.strftime('%m/%d/%Y')
+            if row[index-1] != ''
+                crime_date = datetime.strptime(row[index-1], '%m/%d/%Y')
+                crime_date += timedelta(days=1)
+                row[index-1] = crime_date.strftime('%m/%d/%Y')
     return row
 
 def dump_csv(row):
