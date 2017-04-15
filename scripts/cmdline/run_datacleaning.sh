0c19d50e18c40bad45eb990977a69eb95dd12254
ryodream <shiyanch@gmail.com>
2017-04-15 16:46:49 -0400

    [cmdline] Added cmdline scripts

--- scripts/cmdline/run_datacleaning.sh
+++ scripts/cmdline/run_datacleaning.sh
@@ -8,6 +8,23 @@ dataset='nypd_crime.csv'
 #/usr/bin/hadoop fs -rm cleared_dataset.csv
 #/usr/bin/hadoop fs -put ../../output/filter_out_blank_columns.csv cleared_dataset.csv
 
+# Replace dawn hour
+#bash run_replace_dawn_hour.sh cleaning replace_dawn_hour $dataset
+#/usr/bin/hadoop fs -rm cleared_dataset.csv
+#/usr/bin/hadoop fs -put ../../output/replace_dawn_hour.csv cleared_dataset.csv
+
+dataset='cleared_dataset.csv'
+# Find Invalid Datetime information in the dataset
+#bash run_spark.sh cleaning find_invalid_datetime $dataset
+
+# Filter out invalid datetime inforation in the dataset
+#bash run_filter_out_invalid_datetime.sh cleaning filter_out_invalid_datetime $dataset
+#/usr/bin/hadoop fs -rm cleared_dataset.csv
+#/usr/bin/hadoop fs -put ../../output/filter_out_invalid_datetime.csv cleared_dataset.csv
+
+# Validate report date
+bash run_spark.sh cleaning validate_report_date $dataset
+
 dataset='cleared_dataset.csv'
 # Replace blank cells to 'undefined' in each columns
 #bash run_replace_blank_columns.sh cleaning replace_blank_columns $dataset
@@ -27,7 +44,7 @@ dataset='cleared_dataset.csv'
 # bash run_replace_dawn_hour.sh cleaning replace_dawn_hour $dataset
 
 #/usr/bin/hadoop fs -rm cleared_dataset.csv
-#/usr/bin/hadoop fs -put ../../output/filter_out_by_year.csv cleared_dataset.csv
+#/usr/bin/hadoop fs -put ../../output/replace_dawn_hour.csv cleared_dataset.csv
 
 # Check if the key is duplicated
 #bash run_spark.sh cleaning check_duplicate $dataset
@@ -37,10 +54,10 @@ dataset='cleared_dataset.csv'
 
 # Validate format for particular column
 # Validate CMPLNT_FR_TM
-bash run_validate_format.sh cleaning validate_format $dataset 2 '(2[0-3]|1[0-9]|0[0-9]|[^0-9][0-9]):([0-5][0-9]|[0-9]):([0-5][0-9]|[0-9])'
+#bash run_validate_format.sh cleaning validate_format $dataset 2 '(2[0-3]|1[0-9]|0[0-9]|[^0-9][0-9]):([0-5][0-9]|[0-9]):([0-5][0-9]|[0-9])'
 
 # Validate CMPLNT_TO_TM
-bash run_validate_format.sh cleaning validate_format $dataset 4 '(2[0-3]|1[0-9]|0[0-9]|[^0-9][0-9]):([0-5][0-9]|[0-9]):([0-5][0-9]|[0-9])'
+#bash run_validate_format.sh cleaning validate_format $dataset 4 '(2[0-3]|1[0-9]|0[0-9]|[^0-9][0-9]):([0-5][0-9]|[0-9]):([0-5][0-9]|[0-9])'
 
 # Validate CMPLNT_FR_DT
 #bash run_validate_date.sh cleaning validate_date cleared_dataset.csv 1
