7e965b9bf888f85bcdb0b28374193c110b93661a
ryodream <shiyanch@gmail.com>
2017-05-10 00:12:43 -0400

    [update] update output

--- scripts/cmdline/run_weather.sh
+++ scripts/cmdline/run_weather.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-bash run_spark.sh correlation weather_average weather.csv
+bash run_spark.sh correlation weather_average_by_day weather.csv
+bash run_spark.sh correlation weather_average_by_month weather.csv
+bash run_spark.sh correlation weather_average_by_year  weather.csv
+
+
+
+
